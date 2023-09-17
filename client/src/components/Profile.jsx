1d9a0f9d7185a7c50d7c515759be12d827a76821
srynsh <suryaansh2003@gmail.com>
2023-09-17 08:23:00 +0000

    commited profiles issues

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -27,52 +27,54 @@ const Profile = () => {
 
   //console.log(input_event)
   useEffect(() => {
-   const fetchData = async() => {
-    await fetch("/events.json",{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }})
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      const inputList = data.inputList;
-      esetoptions(inputList);
+    const fetchData = async () => {
+      await fetch("/events.json", {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          const inputList = data.inputList;
+          esetoptions(inputList);
 
-      // You can use 'inputList' here or perform any other operations with it
-    })
-    .catch((error) => {
-      console.error("Error fetching or parsing JSON:", error);
-    });
+          // You can use 'inputList' here or perform any other operations with it
+        })
+        .catch((error) => {
+          console.error("Error fetching or parsing JSON:", error);
+        });
 
 
-  await fetch("/teams.json",{
-    headers : { 
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-     }})
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      const inputList = data.inputList;
-      tsetoptions(inputList);
+      await fetch("/teams.json", {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          const inputList = data.inputList;
+          tsetoptions(inputList);
 
-      // You can use 'inputList' here or perform any other operations with it
-    })
-    .catch((error) => {
-      console.error("Error fetching or parsing JSON:", error);
-    });
-   }
+          // You can use 'inputList' here or perform any other operations with it
+        })
+        .catch((error) => {
+          console.error("Error fetching or parsing JSON:", error);
+        });
+    }
 
-   fetchData()
+    fetchData()
   }, [])
   //User Details Import from Backend
 
@@ -85,13 +87,13 @@ const Profile = () => {
   });
 
   useEffect(() => {
-    setLoading(true);                
+    setLoading(true);
     axios
       .get(import.meta.env.VITE_BACKEND_URL + "profile", {
         withCredentials: true,
       })
       .then((response) => {
-         //res.auth is false then redirect to login page
+        //res.auth is false then redirect to login page
         if (!response.data.auth) {
           window.location.href = "/login";
         }
@@ -99,7 +101,7 @@ const Profile = () => {
         //console.log("GIVING DATA", response.data);
         const userData = response.data.user;
         // Update the User state with user details
-     
+
 
         setUser({
           avatar: userData.avatar_url,
@@ -109,21 +111,21 @@ const Profile = () => {
           events: userData.preferedEvents,
         });
 
-  
-      
+
+
       })
 
       .catch((error) => {
         console.error("Error fetching user details: ", error);
       });
 
-            //set the valid variable  to true if the user has already selected the events and teams
-            setTimeout(() => {
-              setLoading(false);
-            }, 1300);
-      
+    //set the valid variable  to true if the user has already selected the events and teams
+    setTimeout(() => {
+      setLoading(false);
+    }, 1300);
 
-  }, []); 
+
+  }, []);
 
   // Handling selections/Changes in the Form
 
@@ -167,24 +169,24 @@ const Profile = () => {
       alert("Please select atleast one Event!");
       return;
     }
-    else if  (User.supportedTeams.length == 0) {
+    else if (User.supportedTeams.length == 0) {
       alert("Please select atleast one Team!");
       return;
     }
-   else{
-    axios
-    .post(import.meta.env.VITE_BACKEND_URL + "profile/update", User, {
-      withCredentials: true,
-    })
-    .then((data) => {
-      if (data.status === 200) {
-        alert("Profile Updated Successfully");
-      } else {
-        alert("Error updating profile");
-      }
-    });
-    console.log("done");
-   }
+    else {
+      axios
+        .post(import.meta.env.VITE_BACKEND_URL + "profile/update", User, {
+          withCredentials: true,
+        })
+        .then((data) => {
+          if (data.status === 200) {
+            alert("Profile Updated Successfully");
+          } else {
+            alert("Error updating profile");
+          }
+        });
+      console.log("done");
+    }
   };
 
   const validateName = (name) => {
@@ -216,124 +218,121 @@ const Profile = () => {
     <div>
       {loading ? (<Loading />) : (
         <>
-        <div className="container">
-        <h1 className="">Profile Details</h1>
-        <form onSubmit={handleSubmit} name="myForm" className="">
-          <div className="layer1">
-            <div className="form-group">
-              <label htmlFor="name" className="form-label">
-                Name: &nbsp;
-              </label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                className="inputborder"
-                onChange={handleChange}
-                value={User.name}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="Email" className="form-label ">
-                Email: &nbsp;
-              </label>
-              <input
-                type="text"
-                name="Email"
-                id="Email"
-                className="inputborder"
-                value={User.email}
-                disabled
-              />
-            </div>
-          </div>
-          <div className=" layer-2">
-            <div className="form-group"
-              onFocus={handleBlocksMultiselectFocus}
-              onBlur={handleBlocksMultiselectBlur}>
-              <label htmlFor="supportingTeams" className="form-label">
-                Block: &nbsp;
-              </label>
-              <div style={{ transition: 'all 1s ease-in' }}>
-                <Multiselect
-                  name="supportingTeams"
-                  id="supportingTeams"
-                  isObject={false}
-                  placeholder={'Search Team'}
-                  displayValue="supportingTeams"
-                  options={toptions}
-                  onSelect={handleBlocksSelect}
-                  onRemove={handleBlocksRemove}
-                  showCheckbox
-                  className="inputborder custom-multiselect-container"
-                  showArrow
-                  groupBy="category"
-                  selectionLimit={1}
-                  hidePlaceholder={true}
-                  selectedValues={User.supportedTeams}
-                  style={{
-                    multiselectContainer: {
-                      marginBottom: isBlocksMultiselectClicked ? "240px" : "0",
-                      transition: "margin 1s ease-in-out",
-                    },
-                    searchWrapper:{
-                      height:'27px',
-                    }
-                  }}
-                />
+          <div className="container">
+            <h1 className="">Profile Details</h1>
+            <form onSubmit={handleSubmit} name="myForm" className="">
+              <div className="layer1">
+                <div className="form-group">
+                  <label htmlFor="name" className="form-label">
+                    Name: &nbsp;
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="inputborder"
+                    onChange={handleChange}
+                    value={User.name}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="Email" className="form-label ">
+                    Email: &nbsp;
+                  </label>
+                  <input
+                    type="text"
+                    name="Email"
+                    id="Email"
+                    className="inputborder"
+                    value={User.email}
+                    disabled
+                  />
+                </div>
               </div>
-            </div>
-            <div className="form-group"
-              onFocus={handleEventsMultiselectFocus}
-              onBlur={handleEventsMultiselectBlur}>
-              <label htmlFor="Events" className="form-label">
-                Events: &nbsp;
-              </label>
-              <div className="">
-                <Multiselect
-                  name="events"
-                  id="events"
-                  isObject={false}
-                  options={eoptions}
-                  onSelect={handleEventsSelect}
-                  onRemove={handleEventsRemove}
-                  displayValue="name"
-                  placeholder={"Search Events"}
-                  showCheckbox
-                  className="inputborder custom-multiselect-container"
-                  showArrow
-                  selectedValues={User.events}
-                  groupBy="category"
-                  style={{
-                    multiselectContainer: {
-                      marginBottom: isEventsMultiselectClicked ? "240px" : "0",
-                      transition: "margin 1s ease-in-out",
-                    },
-                    optionContainer: {  
-                      border:'2px solid',
-                    },
-                    searchWrapper:{
-                      height:'32px',
-                    },chips:{
-                      display:'none',
-                    },  
-                  }}
-                />
+              <div className=" layer-2">
+                <div className="form-group"
+                  onFocus={handleBlocksMultiselectFocus}
+                  onBlur={handleBlocksMultiselectBlur}>
+                  <label htmlFor="supportingTeams" className="form-label">
+                    Block: &nbsp;
+                  </label>
+                  <div style={{ transition: 'all 1s ease-in' }}>
+                    <Multiselect
+                      name="supportingTeams"
+                      id="supportingTeams"
+                      isObject={false}
+                      placeholder={'Search Team'}
+                      displayValue="supportingTeams"
+                      options={toptions}
+                      onSelect={handleBlocksSelect}
+                      onRemove={handleBlocksRemove}
+                      showCheckbox
+                      className="inputborder custom-multiselect-container"
+                      showArrow
+                      groupBy="category"
+                      selectionLimit={1}
+                      hidePlaceholder={true}
+                      selectedValues={User.supportedTeams}
+                      style={{
+                        multiselectContainer: {
+                          marginBottom: isBlocksMultiselectClicked ? "240px" : "0",
+                          transition: "margin 1s ease-in-out",
+                        },
+                        searchWrapper: {
+                          height: '27px',
+                        }
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className="form-group" onFocus={handleEventsMultiselectFocus} onBlur={handleEventsMultiselectBlur}>
+                  <label htmlFor="Events" className="form-label">
+                    Events: &nbsp;
+                  </label>
+                  <div className="multiselect-container"> {/* Wrap Multiselect in a container */}
+                    <Multiselect
+                      name="events"
+                      id="events"
+                      isObject={false}
+                      options={eoptions}
+                      onSelect={handleEventsSelect}
+                      onRemove={handleEventsRemove}
+                      displayValue="name"
+                      placeholder={"Search Events"}
+                      showCheckbox
+                      className="inputborder custom-multiselect-container"
+                      showArrow
+                      selectedValues={User.events}
+                      groupBy="category"
+                      style={{
+                        multiselectContainer: {
+                          marginBottom: isEventsMultiselectClicked ? "240px" : "0",
+                          transition: "margin 1s ease-in-out",
+                        },
+                        optionContainer: {
+                          border: '2px solid',
+                        },
+                        searchWrapper: {
+                          height: '32px',
+                        },
+                        chips: {
+                          display: "none"
+                        },
+                      }}
+                    />
+                  </div>
+                </div>
+                <button type="submit" onSubmit={handleSubmit} className="submit-button">
+                  Submit
+                </button>
+
               </div>
-            </div>
+            </form>
           </div>
-        </form>
-      </div>
-                      <div>
-                      <button
-                        type="submit"
-                        onSubmit={handleSubmit}
-                        className="submit-button"
-                      >
-                        Submit
-                      </button>
-                    </div>
-      </>
+          <div>
+
+          </div>
+        </>
       )}
     </div>
   );
