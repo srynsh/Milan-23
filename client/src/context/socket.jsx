d524e97b58f89c4c292448dfde66a2690bb11d36
ajaykrshna <ajaykfan@gmail.com>
2023-09-17 12:34:39 +0530

    solved issues in socket

--- client/src/context/socket.jsx
+++ client/src/context/socket.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import io from "socket.io-client";
-import { SOCKET_URL } from "../config";
 
-export const socket = io(import.meta.env.VITE_BACKEND_URL);
+export const socket = io(import.meta.env.VITE_SOCKET_URL);
 const SocketContext = React.createContext(socket);
 export default SocketContext;       
