afdf9f589510f85dd81bc5602ca523dc56ce6b2c
ajaykrshna <ajaykfan@gmail.com>
2023-09-21 13:30:31 +0530

    added loading screen for blockrace

--- client/src/components/BlockRace.jsx
+++ client/src/components/BlockRace.jsx
@@ -1,49 +1,30 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import "../mainpage.css";
 
-import '../mainpage.css'
-const BlockRace = () => {
+function BlockRace() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  function handleIframeLoad() {
+    setIsLoading(false);
+  }
+
   return (
     <>
-      <EmbeddedContent />
+      {isLoading && <div style={{ height: "1.5vh", width: "100%" }} />}
+      {isLoading && <Loading />}
+      <div className="blockrace">
+        <iframe
+          src="https://flo.uri.sh/visualisation/14854093/embed"
+          title="Interactive or visual content"
+          className="flourish-embed-iframe"
+          onLoad={handleIframeLoad}
+          style={{ display: isLoading ? "none" : "block" }}
+          // style={{ width: "100%", height: "600px" }}
+          sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
+        ></iframe>
+      </div>
     </>
-  );
-};
-
-function EmbeddedContent() {
-  return (
-    <div className="blockrace">
-      <iframe
-        src="https://flo.uri.sh/visualisation/14854093/embed"
-        title="Interactive or visual content"
-        className="flourish-embed-iframe"
-        // style={{ width: "100%", height: "600px" }}
-        sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
-      ></iframe>
-      {/* <div
-        style={{
-          width: "100%",
-          marginTop: "4px",
-          textAlign: "right",
-        }}
-      >
-        <a
-          className="flourish-credit"
-          href="https://public.flourish.studio/visualisation/14854093/?utm_source=embed&utm_campaign=visualisation/14854093"
-          target="_top"
-          style={{ textDecoration: "none" }}
-        >
-          <img
-            alt="Made with Flourish"
-            src="https://public.flourish.studio/resources/made_with_flourish.svg"
-            style={{
-              width: "105px",
-              height: "16px",
-              border: "none",
-              margin: "0",
-            }}
-          />
-        </a>
-      </div> */}
-    </div>
   );
 }
 
